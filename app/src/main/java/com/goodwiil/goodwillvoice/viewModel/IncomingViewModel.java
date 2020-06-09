4f824087c45477b66d84e142fa075349d8b5b966
mjkim <eoqkrskfk94@naver.com>
2020-06-10 01:23:36 +0900

    스플레쉬 화면 등록 화면

---
+++ app/src/main/java/com/goodwiil/goodwillvoice/viewModel/IncomingViewModel.java
@@ -0,0 +1,34 @@
+package com.goodwiil.goodwillvoice.viewModel;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.TelephonyManager;
+
+public class IncomingViewModel extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        //전화 상태 받아오기
+        String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+
+
+        //전화가 울릴때
+        if(state.equalsIgnoreCase(TelephonyManager.EXTRA_STATE_RINGING)){
+
+        }
+
+        //전화를 받았을때, 전화를 걸때
+        if(state.equalsIgnoreCase(TelephonyManager.EXTRA_STATE_OFFHOOK)){
+
+        }
+
+        //전화를 끊었을때
+        if(state.equalsIgnoreCase(TelephonyManager.EXTRA_STATE_IDLE)){
+
+        }
+
+    }
+}
