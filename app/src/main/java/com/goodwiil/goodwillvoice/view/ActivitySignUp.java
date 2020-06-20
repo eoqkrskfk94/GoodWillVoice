7e3202ab7adc1fdf8ff059a4aa2bfde3f92d48fc
mjkim <eoqkrskfk94@naver.com>
2020-06-20 23:13:30 +0900

    spinner populate

--- app/src/main/java/com/goodwiil/goodwillvoice/view/ActivitySignUp.java
+++ app/src/main/java/com/goodwiil/goodwillvoice/view/ActivitySignUp.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import com.goodwiil.goodwillvoice.R;
 import com.goodwiil.goodwillvoice.databinding.ActivitySignUpBinding;
 import com.goodwiil.goodwillvoice.model.User;
 import com.goodwiil.goodwillvoice.viewModel.SignUpViewModel;
+
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class ActivitySignUp extends AppCompatActivity {
 
@@ -16,6 +20,7 @@ public class ActivitySignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         createBinding();
+        populateSpinner();
 
 
     }
@@ -26,9 +31,19 @@ public class ActivitySignUp extends AppCompatActivity {
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up);
         mBinding.setViewModel(new SignUpViewModel());
         mBinding.setModel(new User());
+    }
 
-        //mBinding.getViewModel().yearSpinner(this,mBinding.yearSpinner);
+    private void populateSpinner(){
+        ArrayList<String> years = new ArrayList<String>();
 
+        years.add("출생년도");
 
+        int thisYear = Calendar.getInstance().get(Calendar.YEAR);
+        for (int i = thisYear; i >= 1900; i--) {
+            years.add(Integer.toString(i));
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,years);
+        mBinding.spYear.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/goodwiil/goodwillvoice/viewModel/MenuViewModel.java
+++ app/src/main/java/com/goodwiil/goodwillvoice/viewModel/MenuViewModel.java
@@ -1,7 +1,15 @@
 package com.goodwiil.goodwillvoice.viewModel;
 
 
+import android.app.Activity;
+import android.view.View;
+
+
 public class MenuViewModel{
+
+    public void backBtnClick(View view){
+        ((Activity)view.getContext()).finish();
+    }
 
 
 }
