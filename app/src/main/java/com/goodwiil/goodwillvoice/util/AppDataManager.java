de8e65b69ded6f77ca91d86edea8278a219cee33
mjkim <eoqkrskfk94@naver.com>
2020-06-27 23:46:09 +0900

    settings preference

--- app/src/main/java/com/goodwiil/goodwillvoice/util/AppDataManager.java
+++ app/src/main/java/com/goodwiil/goodwillvoice/util/AppDataManager.java
@@ -13,13 +13,15 @@ public class AppDataManager {
     public static final String SP_NAME = "userInfo";
     public static final String SP_KEY_USER = "userInfoKey";
 
-    public static final String SETTINGS = "settingInfo";
-    public static final String SETTINGS_KEY = "settingInfoKey";
+    public static final String SETTING_VIBRATE = "vibration_alarm";
+    public static final String SETTING_VOICE = "voice_alarm";
 
     public static final String SP_NAME_YEAR = "year";
     public static final String SP_NAME_GENDER = "gender";
     public static final String SP_NAME_CAREER = "career";
     public static final String SP_NAME_CITY = "city";
+
+
 
     //shared preference에 값을 저장하기
     public static void setSharedPrefs(String name, User user){
@@ -32,15 +34,6 @@ public class AppDataManager {
 
     }
 
-    public static void setSharedPrefs(String name, Setting setting){
-        SharedPreferences prefs = GoodWillApplication.getContext().getSharedPreferences(name, Activity.MODE_PRIVATE);
-        SharedPreferences.Editor editor = prefs.edit();
-        Gson gson = new Gson();
-        String json = gson.toJson(setting);
-        editor.putString(SETTINGS_KEY, json);
-        editor.apply();
-
-    }
 
     //shared preference에 값을 불러오기
     public static SharedPreferences getSharedPrefs(String name){
@@ -52,10 +45,6 @@ public class AppDataManager {
         return (gson.fromJson(getSharedPrefs(SP_NAME).getString(SP_KEY_USER, null), User.class));
     }
 
-    public static User getSettingModel(){
-        Gson gson = new Gson();
-        return (gson.fromJson(getSharedPrefs(SETTINGS).getString(SETTINGS_KEY, null), User.class));
-    }
 
 
 
