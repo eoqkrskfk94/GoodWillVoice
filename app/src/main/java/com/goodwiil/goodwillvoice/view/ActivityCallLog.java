766cec9f03612b014ce7ad23b8ace8f422794473
mjkim <eoqkrskfk94@naver.com>
2020-06-29 22:51:18 +0900

    FAQ, 고객센터 화면 추가

---
+++ app/src/main/java/com/goodwiil/goodwillvoice/view/ActivityCallLog.java
@@ -0,0 +1,26 @@
+package com.goodwiil.goodwillvoice.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.goodwiil.goodwillvoice.R;
+import com.goodwiil.goodwillvoice.databinding.ActivityCallCenterBinding;
+import com.goodwiil.goodwillvoice.viewModel.CallLogViewModel;
+
+public class ActivityCallLog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        createBinding();
+    }
+
+
+    private ActivityCallCenterBinding mBinding;
+    private void createBinding(){
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_call_log);
+        mBinding.setViewModel(new CallLogViewModel());
+    }
+}
--- app/src/main/java/com/goodwiil/goodwillvoice/viewModel/MainViewModel.java
+++ app/src/main/java/com/goodwiil/goodwillvoice/viewModel/MainViewModel.java
@@ -5,12 +5,17 @@ import android.view.View;
 
 import com.goodwiil.goodwillvoice.util.AppDataManager;
 import com.goodwiil.goodwillvoice.util.ScreenManager;
+import com.goodwiil.goodwillvoice.view.ActivityCallLog;
 import com.goodwiil.goodwillvoice.view.ActivityMenu;
 
 public class MainViewModel extends BaseViewModel{
 
     public void menuBtnClick(View view){
         ScreenManager.startActivity(view.getContext(), ActivityMenu.class);
+    }
+
+    public void callLogBtnClick(View view){
+        ScreenManager.startActivity(view.getContext(), ActivityCallLog.class);
     }
 
     public void setPermissionPref(String name, Boolean auth){
