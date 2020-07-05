d1f6de449b43a449a018a5eef1ef62ee2b75b8dc
mjkim <eoqkrskfk94@naver.com>
2020-07-05 22:28:07 +0900

    팝업창 구현

--- app/src/main/java/com/goodwiil/goodwillvoice/view/ServiceIncoming.java
+++ app/src/main/java/com/goodwiil/goodwillvoice/view/ServiceIncoming.java
@@ -5,8 +5,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.os.IBinder;
+import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
@@ -14,16 +16,22 @@ import android.view.WindowManager;
 import com.goodwiil.goodwillvoice.R;
 import com.goodwiil.goodwillvoice.databinding.ServiceIncomingBinding;
 import com.goodwiil.goodwillvoice.model.IncomingNumber;
+import com.goodwiil.goodwillvoice.util.CallLogDataManager;
 import com.goodwiil.goodwillvoice.viewModel.IncomingViewModel;
 
 
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 
+
 public class ServiceIncoming extends Service {
 
     WindowManager wm;
     View mView;
+    WindowManager.LayoutParams params;
+    private int MAX_X = -1, MAX_Y = -1;
+    private float  START_Y;							//움직이기 위해 터치한 시작 점
+    private int  PREV_Y;								//움직이기 이전에 뷰가 위치한 점
 
 
 
@@ -36,11 +44,12 @@ public class ServiceIncoming extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         createBinding();
 
-        WindowManager.LayoutParams params = setParams();
+        params = setParams();
 
         mView = mBinding.getRoot();
+        mView.setOnTouchListener(mViewTouchListener);
         wm = (WindowManager) getSystemService(WINDOW_SERVICE);
-        mBinding.textView.setText((String) intent.getExtras().get("incomingNumber"));
+        mBinding.tvNumber.setText(CallLogDataManager.convertNumber((String) intent.getExtras().get("incomingNumber")));
         wm.addView(mView, params);
 
 
@@ -71,9 +80,15 @@ public class ServiceIncoming extends Service {
 
     private void createBinding(){
         LayoutInflater inflate = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        mBinding = DataBindingUtil.inflate(inflate, R.layout.service_incoming, null, false);
+        mBinding = DataBindingUtil.inflate(inflate, R.layout.service_incoming, null, true);
+
         mBinding.setViewModel(new IncomingViewModel());
         mBinding.setModel(new IncomingNumber());
+        mBinding.ivProfile.setImageResource(R.drawable.profile_circle);
+        mBinding.ivLine.setImageResource(R.drawable.line);
+        mBinding.tvName.setText("알 수 없는번호");
+        mBinding.tvRestrict.setText("차단이력 00건");
+        mBinding.tvEnd.setText("바로 차단하기");
 
     }
 
@@ -93,6 +108,41 @@ public class ServiceIncoming extends Service {
         params.gravity = Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL;
 
         return params;
+    }
+
+
+    private View.OnTouchListener mViewTouchListener = new View.OnTouchListener() {
+        @Override public boolean onTouch(View v, MotionEvent event) {
+            switch(event.getAction()) {
+                case MotionEvent.ACTION_DOWN:                //사용자 터치 다운이면
+                    if(MAX_X == -1)
+                        setMaxPosition();
+                    START_Y = event.getRawY();                    //터치 시작 점
+                    PREV_Y = params.y;                            //뷰의 시작 점
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    int y = (int)(event.getRawY() - START_Y);	//이동한 거리
+
+                    //터치해서 이동한 만큼 이동 시킨다
+                    params.y = PREV_Y + y;
+
+                    //optimizePosition();        //뷰의 위치 최적화
+                    wm.updateViewLayout(mView, params);    //뷰 업데이트
+                    break;
+            }
+
+            return true;
+        }
+    };
+
+    /**
+     * 뷰의 위치가 화면 안에 있게 최대값을 설정한다
+     */
+    private void setMaxPosition() {
+        DisplayMetrics matrix = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(matrix);		//화면 정보를 가져와서
+
+        MAX_Y = matrix.heightPixels - mView.getHeight();			//y 최대값 설정
     }
 
 
