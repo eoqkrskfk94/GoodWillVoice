ba75abc077e62eba6b60c5f26efff6de90f3afb5
mjkim <eoqkrskfk94@naver.com>
2020-07-09 21:25:34 +0900

    팝업 바인딩 정리

--- app/src/main/java/com/goodwiil/goodwillvoice/viewModel/CallLogViewModel.java
+++ app/src/main/java/com/goodwiil/goodwillvoice/viewModel/CallLogViewModel.java
@@ -3,7 +3,7 @@ package com.goodwiil.goodwillvoice.viewModel;
 import android.app.Activity;
 import android.view.View;
 
-public class CallLogViewModel extends CallCenterViewModel {
+public class CallLogViewModel {
 
     public void backBtnClick(View view){
         ((Activity)view.getContext()).finish();
