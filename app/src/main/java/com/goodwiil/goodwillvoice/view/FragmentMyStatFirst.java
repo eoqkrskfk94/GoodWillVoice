c0ed95d1fbd8b0e9e110b0ac76569fd9ea3c7caa
김명진 <mobinity.kmj@gmail.com>
2020-09-10 00:06:17 +0900

    bottom nav and fragments viewpager

--- app/src/main/java/com/goodwiil/goodwillvoice/view/FragmentMyStatFirst.java
+++ app/src/main/java/com/goodwiil/goodwillvoice/view/FragmentMyStatFirst.java
@@ -1,5 +1,6 @@
 package com.goodwiil.goodwillvoice.view;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,16 @@ import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.IValueFormatter;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.ViewPortHandler;
 import com.goodwiil.goodwillvoice.R;
 import com.goodwiil.goodwillvoice.adapter.CardAdapter;
 import com.goodwiil.goodwillvoice.adapter.CardPagerAdapter;
@@ -16,6 +27,8 @@ import com.goodwiil.goodwillvoice.databinding.FragmentMyStatBinding;
 import com.goodwiil.goodwillvoice.databinding.FragmentMyStatFirstBinding;
 import com.goodwiil.goodwillvoice.model.CardItem;
 import com.goodwiil.goodwillvoice.util.ShadowTransformer;
+
+import java.util.ArrayList;
 
 
 public class FragmentMyStatFirst extends Fragment {
@@ -33,7 +46,44 @@ public class FragmentMyStatFirst extends Fragment {
         mCardView = (CardView) view.findViewById(R.id.cardView);
         mCardView.setMaxCardElevation(mCardView.getCardElevation()
                 * CardAdapter.MAX_ELEVATION_FACTOR);
-        return view;
+
+        mBinding = FragmentMyStatFirstBinding.inflate(inflater, container, false);
+        //mBinding.setViewModel(new SplashViewModel());
+        mBinding.setLifecycleOwner(getActivity());
+
+
+        mBinding.pcCallLog.getDescription().setEnabled(false);
+        mBinding.pcCallLog.setExtraOffsets(5,0,5,0);
+        mBinding.pcCallLog.setTouchEnabled(false);
+        mBinding.pcCallLog.animateY(1000, Easing.EaseInOutCubic);
+        //mBinding.pcCallLog.setDragDecelerationFrictionCoef(0.95f);
+
+        mBinding.pcCallLog.setDrawHoleEnabled(true);
+        mBinding.pcCallLog.setHoleColor(Color.WHITE);
+        mBinding.pcCallLog.setTransparentCircleRadius(61f);
+        mBinding.pcCallLog.getLegend().setEnabled(false);
+
+        ArrayList<PieEntry> yValues = new ArrayList<>();
+
+        yValues.add(new PieEntry(4.0f, ""));
+        yValues.add(new PieEntry(36.0f, ""));
+        yValues.add(new PieEntry(60.0f, ""));
+
+        PieDataSet dataSet = new PieDataSet(yValues, "");
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+        dataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+
+
+        PieData data = new PieData(dataSet);
+        data.setValueTextSize(10f);
+        data.setValueTextColor(Color.WHITE);
+        data.setValueFormatter(new MyValueFormatter());
+
+        mBinding.pcCallLog.setData(data);
+
+
+        return mBinding.getRoot();
     }
 
 
@@ -44,5 +94,15 @@ public class FragmentMyStatFirst extends Fragment {
     }
 
     public CardView getCardView() { return mCardView; }
+
+
+    private class MyValueFormatter extends ValueFormatter {
+
+
+        @Override
+        public String getFormattedValue(float value) {
+            return super.getFormattedValue(value) + " %";
+        }
+    }
 
 }
