d8d42606b77be25e1609f40afec1cfac50427e96
김명진 <mobinity.kmj@gmail.com>
2020-11-11 00:22:59 +0900

    [Add] 네트워크 연결 없을때 경고창 구현

--- app/src/main/java/com/goodwiil/goodwillvoice/viewModel/SplashViewModel.java
+++ app/src/main/java/com/goodwiil/goodwillvoice/viewModel/SplashViewModel.java
@@ -2,6 +2,7 @@ package com.goodwiil.goodwillvoice.viewModel;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -9,14 +10,22 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.PowerManager;
 import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import com.goodwiil.goodwillvoice.R;
 import com.goodwiil.goodwillvoice.util.AppDataManager;
 import com.goodwiil.goodwillvoice.util.ScreenManager;
 import com.goodwiil.goodwillvoice.view.ActivityMain;
 import com.goodwiil.goodwillvoice.view.ActivityMain2;
 import com.goodwiil.goodwillvoice.view.ActivitySignUp;
+
+import java.util.ArrayList;
 
 public class SplashViewModel {
 
@@ -36,19 +45,6 @@ public class SplashViewModel {
     public void startApp(Context context) {
         //화원등록이 되어있으면 바로 메인화면으로 시작 아니면 회원등록화면으로 시작
         ScreenManager.startActivity(context, (AppDataManager.getUserModel() == null) ? (ActivitySignUp.class) : (ActivityMain2.class));
-    }
-
-    public void getPermission(Context context){
-
-        this.context = context;
-        //전화 상태 권한 받기
-        checkPermission();
-
-        //배터리 최적화 권한 받기
-        checkPermissionBattery();
-
-        //앱 위에 그리기 권한 받기
-        checkPermissionOverlay();
     }
 
 
@@ -94,6 +90,86 @@ public class SplashViewModel {
                 ((Activity)context).startActivityForResult(intent, 1);
             }
         }
+    }
+
+
+    /**
+     * 권한을 확인하고 권한이 부여되어 있지 않다면 권한을 요청한다.
+     * @return 필요한 권한이 모두 부여되었다면 true, 그렇지 않다면 false
+     */
+    public Boolean checkAndRequestPermissions(Context context){
+
+        this.context = context;
+
+        String[] permissions = {
+                Manifest.permission.READ_PHONE_STATE,
+//            Manifest.permission.RECEIVE_SMS,
+                Manifest.permission.READ_CALL_LOG,
+                Manifest.permission.READ_CONTACTS,
+//            Manifest.permission.READ_SMS,
+                Manifest.permission.ACCESS_FINE_LOCATION};
+
+
+        ArrayList<String> listPermissionsNeeded = new ArrayList<>();
+
+
+        for(int i = 0 ; i < permissions.length; i++) {
+            if (ContextCompat.checkSelfPermission(context, permissions[i]) != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(permissions[i]);
+            }
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            showPermissionInfoDialog(listPermissionsNeeded);
+            return false;
+        }
+
+        return true;
+    }
+
+
+    /**
+     * 권한 설정 화면으로 이동할 지를 선택하는 다이얼로그를 보여준다.
+     */
+    public void showPermissionInfoDialog(ArrayList<String> listPermissionsNeeded) {
+        //before inflating the custom alert dialog layout, we will get the current activity viewgroup
+        ViewGroup viewGroup = ((Activity)context).findViewById(android.R.id.content);
+
+        //then we will inflate the custom alert dialog xml that we created
+        View dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_permission, viewGroup, false);
+
+
+        //Now we need an AlertDialog.Builder object
+        AlertDialog.Builder builder = new AlertDialog.Builder(context, R.style.MyDialogTheme);
+
+        //setting the view of the builder to our custom view that we already inflated
+        builder.setView(dialogView);
+
+        //finally creating the alert dialog and displaying it
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.setCancelable(false);
+        alertDialog.setCanceledOnTouchOutside(false);
+
+        alertDialog.show();
+
+        Button networkDialogCancelBtn = (Button)alertDialog.findViewById(R.id.alertdialog_button);
+
+        networkDialogCancelBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+                alertDialog.cancel();
+
+                checkPermission();
+
+                //배터리 최적화 권한 받기
+                checkPermissionBattery();
+
+                //앱 위에 그리기 권한 받기
+                checkPermissionOverlay();
+
+            }
+        });
     }
 
 
